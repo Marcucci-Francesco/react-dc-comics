49db828dad9fb12e894cd45ac42a28a4cfda553e
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-01-13 20:43:38 +0100

    feat: add Header and Main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,34 @@
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <section className="list">
+        <div className="container-list">
+          <div>
+            <div>
+              <h3>dc comics</h3>
+              <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+              </ul>
+            </div>
+
+            <div>
+              <h3>shop</h3>
+              <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </footer>
   )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,50 @@
+import Digital from '../../public/img/buy-comics-digital-comics.png'
+import Merchandise from '../../public/img/buy-comics-merchandise.png'
+import Shop from '../../public/img/buy-comics-shop-locator.png'
+import Subscriptions from '../../public/img/buy-comics-subscriptions.png'
+import Power from '../../public/img/buy-dc-power-visa.svg'
+
 const Main = () => {
   return (
-    <main>Main</main>
+    <main>
+
+      <section className="text">
+        <div className="container-text">
+          <h2><i class="fa-solid fa-arrow-right"></i> Content goes here <i class="fa-solid fa-arrow-left"></i></h2>
+        </div>
+      </section>
+
+      <section className="icons">
+        <div className="container-icons">
+          <div className='d-flex'>
+            <img src={Digital} alt="" />
+            <p>digital comics</p>
+          </div>
+
+          <div className='d-flex'>
+            <img src={Merchandise} alt="" />
+            <p>dc merchandise</p>
+          </div>
+
+          <div className='d-flex'>
+            <img src={Shop} alt="" />
+            <p>subscription</p>
+          </div>
+
+          <div className='d-flex'>
+            <img src={Subscriptions} alt="" />
+            <p>comic shop locator</p>
+          </div>
+
+          <div className='d-flex'>
+            <img src={Power} alt="" />
+            <p>dc power visa</p>
+          </div>
+        </div>
+
+      </section>
+
+    </main>
   )
 }
 
